07e34aa11ebcb2eaa16182af01528cb0e0033d88
Faneva herintsoa <rherintsoafaneva@gmail.com>
2026-05-26 17:47:21 +0300

    Initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,12 @@
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.route.js"
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+
