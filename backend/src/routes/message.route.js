3a45a32afb17dd1c6d285904677222444b95e2eb
Faneva herintsoa <rherintsoafaneva@gmail.com>
2026-05-27 12:46:51 +0300

    ready for deployment

---
+++ backend/src/routes/message.route.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+const router = express.Router();
+
+router.get("/send", (req, res) => {
+	res.send("Send message endpoint");
+})
+
+
+// router.get("/send", (req, res) => {
+// 	res.send("Send message endpoint");
+// })
+
+export default router;
