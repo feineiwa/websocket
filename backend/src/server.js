dc95ac8eaa5badd2aa0f817b8de522fbef5dbec9
Faneva herintsoa <rherintsoafaneva@gmail.com>
2026-05-28 16:20:57 +0300

    feat: integrate resend service for welcome email functionality and refactor environment variable handling

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,17 +1,15 @@
 import express from "express";
-import dotenv from "dotenv";
 import path from "path";
 
 import authRoutes from "./routes/auth.route.js"
 import messagesRoutes from "./routes/message.route.js";
 import { connectDb } from "./lib/db.js";
-
-dotenv.config();
+import { ENV } from "../src/lib/env.js"
 
 const app = express();
 const __dirname = path.resolve();
 
-const PORT = process.env.PORT || 3001;
+const PORT = ENV.PORT || 3001;
 
 app.use(express.json()); //req.body
 
@@ -19,7 +17,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messagesRoutes);
 
 // make ready for deployment
-if (process.env.NODE_ENV === "production") {
+if (ENV.NODE_ENV === "production") {
 	app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
 	app.get("*", (_, res) => {
