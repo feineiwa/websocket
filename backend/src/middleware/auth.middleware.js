f0440c210bc0672a112de6e4e0480d25c5aa7f10
Faneva herintsoa <rherintsoafaneva@gmail.com>
2026-06-01 16:35:35 +0300

    feat: implement messaging functionality with message model and controllers

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -5,7 +5,6 @@ import { ENV } from "../lib/env.js";
 export const protectRoute = async (req, res, next) => {
 	try {
 		const token = req.cookies.jwt;
-		console.log(token);
 		if (!token) return res.status(401).json({ message: "Unauthorized - No token provided" });
 		
 		const decoded = jwt.verify(token, ENV.JWT_SECRET);
